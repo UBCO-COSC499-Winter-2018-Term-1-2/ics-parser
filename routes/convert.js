7087f7d3ecfc59e696ad92a445bc8c169b87ed28
David Osemwegie <osahonnn@gmail.com>
2019-01-29 16:33:00 -0800

    was able to filter out the bad characters from the strings

--- routes/convert.js
+++ routes/convert.js
@@ -13,7 +13,7 @@ const multer = require('multer')
 
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, './files/')
     },
     filename: function (req, file, cb) {
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage })
 
 
 
@@ -29,18 +29,6 @@ const upload = multer({storage: storage})
 
 
 router.post('/parse', upload.single('calendar'), (req, res) => {
-
-    // const fileLocation = require('../files/david_cal.ics')
-
-    // var data = ical.parseFile('david_cal.ics')
-
-    //const url = "https://calendar.google.com/calendar/ical/osahonnn%40gmail.com/public/basic.ics"
-
-    const url = "localhost:3001/ical.ics"
-
-    const cal = 'data.txt'
-
-    const filename = req.file.filename
 
     const parseFile = path => {
         fs.readFile(path, "utf8", (err, data) => {
@@ -65,8 +53,8 @@ router.post('/parse', upload.single('calendar'), (req, res) => {
 
                         data[i] = {
                             "title": r.SUMMARY,
-                            "desc": r.DESCRIPTION.replace('\\n', " "),
-                            "roomId": r.LOCATION,
+                            "desc": r.DESCRIPTION.replace(/\\/g, "").replace('.nn', "."),
+                            "roomId": r.LOCATION.replace(/\\/g, ""),
                             "startTime": r["DTSTART;TZID=America/Vancouver"].substr(9),
                             "endTime": r["DTEND;TZID=America/Vancouver"].substr(9),
                         }
@@ -79,50 +67,9 @@ router.post('/parse', upload.single('calendar'), (req, res) => {
             });
 
         });
-
-        console.log("Program Ended");
     };
 
     parseFile(req.file.path)
-
-    //parseFile(url)
-
-    // fs.readFile(cal, 'ut8', (err, data) => {
-    //     if (err) {
-    //         console.log(err)
-    //     } else {
-    //         console.log(data.toString())
-    //     }
-    // })
-
-    //res.send(contents)
-
-    // parser.convert(contents, function(err, parsedResponse) {
-    //     if(err) {
-    //         console.log("Error occurred parsing ical data", err);
-    //     }	else {
-    //         res.send(parsedResponse)
-    //         //parsedResponse is the parsed javascript JSON object
-    //     }
-    // });
-
-
-    // var data = ical.parseFile("david_cal.ics", function (err, data) {
-    //     if (err) console.log(err);
-    //     console.log(data);
-    // });
-
-    // res.send(data)
-
-    // ical.fromURL(url, {}, function (err, data) {
-    //     // if (err) {
-    //     //     res.send(err)
-    //     // } else {
-    //     //     res.json(data)
-    //     // }
-
-    //     res.json({ data,err })
-    // })
 
 })
 
